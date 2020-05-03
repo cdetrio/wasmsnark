fd3e73f4a5b3c19190fa5349ee78e2e715078d61
cdetrio <cdetrio@gmail.com>
2020-05-03 16:00:27 -0400

    add build script for bls12

---
+++ tools/buildwasm_bls12381.js
@@ -0,0 +1,36 @@
+const bigInt = require("big-integer");
+const ModuleBuilder = require("wasmbuilder").ModuleBuilder;
+const buildBn128 = require("../src/bls12381/build_bls12381.js");
+const fs = require("fs");
+const path = require("path");
+
+function buildWasm() {
+    const moduleBuilder = new ModuleBuilder();
+    moduleBuilder.setMemory(1000);
+    buildBn128(moduleBuilder);
+
+    const code = moduleBuilder.build();
+
+    fs.writeFileSync(
+            path.join( __dirname, "..", "build", "bls12381.wasm"),
+            code
+        )
+
+    fs.writeFileSync(
+        path.join( __dirname, "..", "build", "bls12381_wasm.js"),
+        `
+            exports.code = Buffer.from("${Buffer.from(code).toString("base64")}", "base64");
+            exports.pq = ${moduleBuilder.modules.f1m.pq};
+            exports.pr = ${moduleBuilder.modules.frm.pq};
+            exports.pG1gen = ${moduleBuilder.modules.bls12381.pG1gen};
+            exports.pG1zero = ${moduleBuilder.modules.bls12381.pG1zero};
+            exports.pG2gen = ${moduleBuilder.modules.bls12381.pG2gen};
+            exports.pG2zero = ${moduleBuilder.modules.bls12381.pG2zero};
+            exports.pOneT = ${moduleBuilder.modules.bls12381.pOneT};
+            exports.prePSize = ${moduleBuilder.modules.bls12381.prePSize};
+            exports.preQSize = ${moduleBuilder.modules.bls12381.preQSize};
+        `
+    );
+}
+
+buildWasm();
